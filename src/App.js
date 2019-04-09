4f1dbdf2f8c387666b380cb571e1af090f63a92d
Alexander Ignácz <ignaczalexander@gmail.com>
2019-04-09 23:10:25 +0200

    parent component

--- src/App.js
+++ src/App.js
@@ -1,28 +1,102 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import {
+  addDictionary,
+  getDictionaries,
+  deleteDictionary
+} from "./actions/dictActions";
+import "./App.css";
+import Dictionaries from "./components/Dictionaries";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+      errors: {}
+    };
+  }
+  componentDidMount() {
+    this.props.getDictionaries();
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.errors) {
+      this.setState({ errors: nextProps.errors });
+    }
+  }
+
+  onChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+  onSubmit = e => {
+    e.preventDefault();
+    const dictData = { name: this.state.name, rows: [{}] };
+    this.props.addDictionary(dictData);
+    //this.props.deleteDictionary(this.state.name);
+  };
   render() {
+    const { dicts } = this.props;
+    const { errors } = this.state;
+    const dictsPlace =
+      dicts.length === 0 ? (
+        <p className="text-center">You have no dictionaries</p>
+      ) : (
+        <div>
+          <h2>Dictionaries</h2>
+          <Dictionaries dictionaries={dicts} />
+        </div>
+      );
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-10">
+            <h1>Dictionary manager</h1>
+            <form onSubmit={this.onSubmit}>
+              <div className="form-row">
+                <div className="form-group col">
+                  <input
+                    className="form-control"
+                    type="text"
+                    name="name"
+                    placeholder="Dictionary name"
+                    onChange={this.onChange}
+                  />
+                  {errors.name && (
+                    <div className="invalid-feedback d-block">
+                      {errors.name}
+                    </div>
+                  )}
+                </div>
+                <div className="form-group col">
+                  <input
+                    type="submit"
+                    value="Create dictionary"
+                    className="btn btn-primary"
+                  />
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+        {dictsPlace}
       </div>
     );
   }
 }
-
-export default App;
+App.propTypes = {
+  addDictionary: PropTypes.func.isRequired,
+  getDictionaries: PropTypes.func.isRequired,
+  deleteDictionary: PropTypes.func.isRequired,
+  dicts: PropTypes.array.isRequired,
+  errors: PropTypes.object.isRequired
+};
+const mapStateToProps = state => ({
+  dicts: state.dictReducer.dicts,
+  errors: state.errors
+});
+export default connect(
+  mapStateToProps,
+  { addDictionary, getDictionaries, deleteDictionary }
+)(App);
