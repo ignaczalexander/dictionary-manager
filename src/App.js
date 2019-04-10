85f5019e6579223e97238a0fda03cc6a29538cb4
Alexander Ignácz <ignaczalexander@gmail.com>
2019-04-10 22:01:38 +0200

    organized App.js

--- src/App.js
+++ src/App.js
@@ -1,87 +1,32 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
-import {
-  addDictionary,
-  getDictionaries,
-  deleteDictionary
-} from "./actions/dictActions";
-import "./App.css";
+import { getDictionaries } from "./actions/dictActions";
 import Dictionaries from "./components/Dictionaries";
 import Navbar from "./components/navbar/Navbar";
+import AddDictionary from "./components/AddDictionary";
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: "",
-      errors: {}
-    };
-  }
   componentDidMount() {
     this.props.getDictionaries();
   }
 
-  componentWillReceiveProps(nextProps) {
-    if (nextProps.errors) {
-      this.setState({ errors: nextProps.errors });
-    }
-  }
-
-  onChange = e => {
-    this.setState({ [e.target.name]: e.target.value });
-  };
-  onSubmit = e => {
-    e.preventDefault();
-    const dictData = { name: this.state.name, rows: [{}] };
-    this.props.addDictionary(dictData);
-    //this.props.deleteDictionary(this.state.name);
-  };
   render() {
     const { dicts } = this.props;
-    const { errors } = this.state;
     const dictsPlace =
       dicts.length === 0 ? (
         <p className="text-center">You have no dictionaries</p>
       ) : (
         <div>
-          <h2>Dictionaries</h2>
+          <h3 className="mb-3">Dictionaries</h3>
           <Dictionaries dictionaries={dicts} />
         </div>
       );
     return (
-      <div className="">
+      <div>
         <Navbar />
-        <div className="container">
-          <div className="row">
-            <div className="col-md-10">
-              <form onSubmit={this.onSubmit}>
-                <div className="form-row">
-                  <div className="form-group col">
-                    <input
-                      className="form-control"
-                      type="text"
-                      name="name"
-                      placeholder="Dictionary name"
-                      onChange={this.onChange}
-                    />
-                    {errors.name && (
-                      <div className="invalid-feedback d-block">
-                        {errors.name}
-                      </div>
-                    )}
-                  </div>
-                  <div className="form-group col">
-                    <input
-                      type="submit"
-                      value="Create dictionary"
-                      className="btn btn-primary"
-                    />
-                  </div>
-                </div>
-              </form>
-            </div>
-          </div>
+        <div className="container p-3">
+          <AddDictionary />
           {dictsPlace}
         </div>
       </div>
@@ -89,9 +34,7 @@ class App extends Component {
   }
 }
 App.propTypes = {
-  addDictionary: PropTypes.func.isRequired,
   getDictionaries: PropTypes.func.isRequired,
-  deleteDictionary: PropTypes.func.isRequired,
   dicts: PropTypes.array.isRequired,
   errors: PropTypes.object.isRequired
 };
@@ -101,5 +44,5 @@ const mapStateToProps = state => ({
 });
 export default connect(
   mapStateToProps,
-  { addDictionary, getDictionaries, deleteDictionary }
+  { getDictionaries }
 )(App);
