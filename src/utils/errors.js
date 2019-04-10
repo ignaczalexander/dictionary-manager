296bb2886cb3e1fd05301613014825cc835b461c
Alexander Ignácz <ignaczalexander@gmail.com>
2019-04-10 21:59:56 +0200

    const error messages

---
+++ src/utils/errors.js
@@ -0,0 +1,4 @@
+export const DUPLICATE_ERROR = "This row has a duplicate";
+export const FORK_ERROR = "Two rows map to different values";
+export const CYCLE_ERROR = "This row is in a cycle with an other row";
+export const CHAIN_ERROR = "This row is in a chain with an other row";
